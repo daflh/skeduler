ac4e5d470e7d7553c5acad5ce3ec002d22b6e0b0
Dafiul Haq <dafiulhaq97@gmail.com>
2019-12-23 19:14:09 +0700

    initial service worker code

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,33 @@
 self.addEventListener('fetch', ()=>{})
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(skipWaiting());
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('push', function(event) {
+    let options = {
+        body: 'Here is a push notification body!',
+        icon: './img/icons/144x144.png',
+        requireInteraction: true
+    };
+    event.waitUntil(
+        self.registration.showNotification('Coragon Push', options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    let data = event.data;
+    let action = event.action;
+    if (action === 'snooze') {
+        console.log("notification snoozed");
+    } else if (action === "done") {
+        console.log("notification done");
+    } else {
+        clients.openWindow('https://coragon.web.app/events');
+    }
+    event.notification.close();
+});
