cc632af3e0ef4b76094761f79aabc1cbb02ed7f1
Dafiul Haq <dafiulhaq97@gmail.com>
2019-12-30 15:08:31 +0700

    adding sw listener to skip waiting

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,9 @@
 self.addEventListener('fetch', ()=>{})
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(skipWaiting());
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(clients.claim());
+});
